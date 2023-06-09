67402f79e0a97aeb58ffc5660b3f013e4640085d
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-06-10 03:11:27 +0800

    Coomit

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ import {
 } from 'react-icons/ai';
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -32,13 +32,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+      <main className="bg-gray-200 px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen" >
           <nav className="py-10 mb-12 flex justify-between  dark:text-white">
             <h1 className="text-xl font-burtons">Developed by JC</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" />
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl hover:text-orange-600" />
+              </li>
+              <li>
+                <button onClick={()=> setDarkMode(!darkMode)} className="bg-gradient-to-r from-orange-400 to-orange-600 hover:text-white md:font-bold text-black px-4 py-2 rounded-md ml-8 dark:text-white dark:hover:text-black">Dark Mode</button>
               </li>
             </ul>
           </nav>
@@ -53,9 +56,9 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-700  dark:text-white">
-            <a href="https://www.instagram.com/johnsonchin1009/"><AiFillInstagram /></a>
-            <a href="https://github.com/JohnsonChin1009"><AiFillGithub /></a>
-            <a href="https://www.linkedin.com/in/johnson-chin-70b865224/"><AiFillLinkedin /></a>
+            <a href="https://www.instagram.com/johnsonchin1009/" className="hover:text-orange-600 hover:scale-110"><AiFillInstagram /></a>
+            <a href="https://github.com/JohnsonChin1009" className="hover:text-orange-600 hover:scale-110"><AiFillGithub /></a>
+            <a href="https://www.linkedin.com/in/johnson-chin-70b865224/" className="hover:text-orange-600 hover:scale-110"><AiFillLinkedin /></a>
             
           </div>
           <div className="flex justify-center">
@@ -128,18 +131,23 @@ export default function Home() {
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
               <Image src={web1} alt="web1" className="rounded-lg object-cover w-full h-full" layout="responsive" />
+              <p className="flex justify-center text-md mt-2 dark:text-white">APU Central Club Web Application (APUCC)</p>
             </div>
             <div className="basis-1/3 flex-1">
               <Image src={web2} alt='web2' className="rounded-lg object-cover w-full h-full" layout="responsive" />
+              <p className="flex justify-center text-md mt-2 dark:text-white">Hotel Room Booking Management System</p>
             </div>
             <div className="basis-1/3 flex-1">
               <Image src={web3} alt='web3' className="rounded-lg object-cover w-full h-full" layout="responsive" />
+              <p className="flex justify-center text-md mt-2 dark:text-white">QRTect Mobile App Design</p>
             </div>
             <div className="basis-1/3 flex-1">
               <Image src={web4} alt='web4' className="rounded-lg object-cover w-full h-full" layout="responsive" />
+              <p className="flex justify-center text-md mt-2 dark:text-white">HoneyBee Physiotherapy Clinic Web Design</p>
             </div>
             <div className="basis-1/3 flex-1">
               <Image src={web5} alt='web5' className="rounded-lg object-cover w-full h-full" layout="responsive" />
+              <p className="flex justify-center text-md mt-2 dark:text-white">Asia Pacific Final Year Project Centre Fully Functioning Web App (ApFYPCentre)</p>
             </div>
           </div>
         </section>
