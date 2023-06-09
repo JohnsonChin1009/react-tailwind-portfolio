cac1458a50f209fd46a2d68dd5fff8f8e8a84ba2
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-06-10 01:26:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
     <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>Johnson Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
